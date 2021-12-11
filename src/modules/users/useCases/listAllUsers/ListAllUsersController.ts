e2ff83056fb69fbc0b1dd272c5f5a484b74069c9
lucas vinicius <lucasviniciusaalves@gmail.com>
2021-12-11 17:32:55 -0300

    fix: list all users route

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -5,10 +5,16 @@ import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.params;
-    const all = this.listAllUsersUseCase.execute({ user_id });
+    const { user_id } = request.headers;
+    try {
+      const all = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
 
-    return response.status(201).json(all);
+      return response.status(201).json(all);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
