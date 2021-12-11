dfb02f4ebd88aca5c9f5e045d9124968a9a53da7
lucas vinicius <lucasviniciusaalves@gmail.com>
2021-12-11 17:24:01 -0300

    fix: route turn user admin

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -11,6 +11,10 @@ class TurnUserAdminUseCase {
   execute({ user_id }: IRequest): User {
     const user = this.usersRepository.findById(user_id);
 
+    if (!user) {
+      throw new Error(`User ${user_id} not found`);
+    }
+
     user.admin = true;
 
     return user;
