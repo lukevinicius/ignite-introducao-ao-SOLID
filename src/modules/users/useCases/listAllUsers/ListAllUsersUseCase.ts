a028b2bec640cdd1550b8d0b3b4ce67cd19bbd2e
lucas vinicius <lucasviniciusaalves@gmail.com>
2021-12-11 17:05:52 -0300

    add: list users

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,20 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("Este usuário não existe");
+    }
+
+    if (!user.admin) {
+      throw new Error("Você não é um administrador");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
